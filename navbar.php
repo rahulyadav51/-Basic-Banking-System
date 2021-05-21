683af05b6ca921052d60d161a2f01d017ccb9169
Rahul Yadav <rahulyadav.cs1@gmail.com>
2021-05-21 21:15:58 +0530

    The Sparks Foundation
    Sparks Foundation Internship Project : Basic Banking System
    A Web Application used to transfer money between multiple users (Project contains 10 dummy users). A dummy user can also be created.
    
    Stack used - Front-end : HTML, CSS, Bootstrap & Javascript Back-end : PHP Database : MySQL
    
    Database contains two Tables- Users Table & Transaction Table
    
    1. User table have basic fields such as name, email & current balance.
    
    2. Transaction table records all transfers happened along with their time.
    
    Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.
    
    Co-Authored-By: Rahul Yadav <78354230+rahulyadav51@users.noreply.github.com>

---
+++ navbar.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: white;
+    }
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+  </style>
+</head>
+
+<body>
+  <!--Responsive Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <div class="container-fluid nav-wrapper #00838f cyan darken-3 z-depth-5" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+
+        <li><a href="index.php">Home</a></li>
+        <li><a href="moneytransfer.php">View Customers</a></li>
+        <li><a href="transactionhistory.php">Transactions</a></li>
+      
+    </ul>
+    <a href="index.php" class=""><img src="https://www.thesparksfoundationsingapore.org/images/logo_small.png" style="height:70px;width:60px" /></a>
+  </div>
+</nav>
+  <!--end of navbar-->
+</body>
+
+</html>
